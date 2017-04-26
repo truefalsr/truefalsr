99e113593eae3c1fb60d9196e76512102edc17f4
truefalsr <axcee.d@gmail.com>
2017-04-26 23:26:40 +0300

    Add test challenges

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,20 +1,24 @@
 tf = {
   nextChallenge: function() {
-
+    var codeElement = document.getElementById('tf-code');
+    codeElement.innerText = this.challenges[Math.floor(Math.random()*this.challenges.length)];
+    hljs.highlightBlock(codeElement);
   },
   challenges: [
-      '<?php $a = true;',
-      '<?php $b = null;'
+      "<?php\n$a = true;\n\n"+
+      "if (true !== $a) {",
+      "<?php\n$b = null;",
+      "<?php\ndefine('SEVEN', 7);"
   ]
 };
 document.addEventListener("DOMContentLoaded", function(event) {
     document.onkeydown = function(e) {
         switch (e.which) {
             case 37:
-                document.getElementById('leftBtn').click();
+                tf.nextChallenge();
                 break;
             case 39:
-                document.getElementById('rightBtn').click();
+                tf.nextChallenge();
                 break
         }
     }
